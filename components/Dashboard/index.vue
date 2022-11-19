b9d277b0a6598d75657decea43b0f088d2730b5e
[200~ <desarrollo@agenciamanager.com>
2022-11-19 11:32:37 -0500

    Subida proyecto

---
+++ components/Dashboard/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <Dashboard/>
+    </div>
+  </template>
+  
+  <script>
+   import Dashboard from "~/components/Dashboard";
+  export default {
+    comments:{
+      Dashboard
+    }
+  }
+  </script>
